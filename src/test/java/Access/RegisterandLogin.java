bf020b36ed56425953ca49e3b38da20384c1a553
femiBalogun <femgee@gmail.com>
2020-03-17 19:13:03 +0000

    more codes added

--- src/test/java/Access/RegisterandLogin.java
+++ src/test/java/Access/RegisterandLogin.java
@@ -4,6 +4,7 @@ import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
 
 import java.util.concurrent.TimeUnit;
@@ -12,7 +13,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterTest;
+import org.openqa.selenium.support.ui.Select;
 
 public class RegisterandLogin {
 
@@ -28,17 +29,17 @@ public class RegisterandLogin {
 
 	@Test
 	public void registrationTest() {
-		// select gender option
-		WebElement gender = driver.findElement(By.id("gender-male"));
-		gender.click();
 
-		// enter first name
-		WebElement first = driver.findElement(By.id("FirstName"));
-		first.sendKeys("Testerg");
-
-		// enter second name
-		WebElement second = driver.findElement(By.id("LastName"));
-		second.sendKeys("Testino1234");
+		driver.findElement(By.id("gender-male")).click();// selected gender as male
+		driver.findElement(By.id("FirstName")).sendKeys("Testerg"); // entered firstname script
+		driver.findElement(By.id("LastName")).sendKeys("Testino1234"); // entered lastname script
+		Select day = new Select(driver.findElement(By.name("DateOfBirthDay")));
+		day.selectByValue("6"); // selected day of birth
+		Select month = new Select(driver.findElement(By.name("DateOfBirthMonth")));
+		month.selectByVisibleText("June"); // selected month of biryh
+		Select year = new Select(driver.findElement(By.name("DateOfBirthYear")));
+		year.selectByValue("1978"); // selected year of birth
+		driver.findElement(By.id("Email")).sendKeys("femgee2009@gmail.com"); // entered year of birth
 
 	}
 
