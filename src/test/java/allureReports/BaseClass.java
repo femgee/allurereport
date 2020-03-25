b9220ed722268c20e91c6c68123a5554c1ba9bdf
femiBalogun <femgee@gmail.com>
2020-03-25 08:32:22 +0000

    BaseClass parameter changed to verify if it works on Firefox

--- src/test/java/allureReports/BaseClass.java
+++ src/test/java/allureReports/BaseClass.java
@@ -2,7 +2,9 @@ package allureReports;
 
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;//
+//import org.openqa.selenium.edge.EdgeDriver;//
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -13,8 +15,8 @@ public class BaseClass {
 
 	public WebDriver initialize_driver() {
 
-		WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver();
+		WebDriverManager.firefoxdriver().setup();
+		driver = new FirefoxDriver();
 		driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 		// driver.manage().window().maximize();//
