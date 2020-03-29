a0c510609aac4bb46f38605752125cad662a4ec3
femiBalogun <femgee@gmail.com>
2020-03-29 13:18:06 +0100

    -a

--- src/test/java/allureReports/BaseClass.java
+++ src/test/java/allureReports/BaseClass.java
@@ -3,9 +3,9 @@ package allureReports;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.chrome.ChromeDriver;//
+import org.openqa.selenium.chrome.ChromeDriver;
 //import org.openqa.selenium.edge.EdgeDriver;//
-import org.openqa.selenium.firefox.FirefoxDriver;
+//import org.openqa.selenium.firefox.FirefoxDriver;//
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -16,8 +16,8 @@ public class BaseClass {
 
 	public WebDriver initialize_driver() {
 
-		WebDriverManager.firefoxdriver().setup();
-		driver = new FirefoxDriver();
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
 		driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 		 //driver.manage().window().setSize(new Dimension(375, 667);//
