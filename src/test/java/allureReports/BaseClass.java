2c60e41c1d6479a1495bff80320aaa0b998eef16
femiBalogun <femgee@gmail.com>
2020-03-31 20:08:26 +0100

    added more parameters to environment.xml

--- src/test/java/allureReports/BaseClass.java
+++ src/test/java/allureReports/BaseClass.java
@@ -3,8 +3,8 @@ package allureReports;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.edge.EdgeDriver;//
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+//import org.openqa.selenium.firefox.FirefoxDriver;//
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -16,7 +16,7 @@ public class BaseClass {
 	public WebDriver initialize_driver() {
 
 		WebDriverManager.firefoxdriver().setup();
-		driver = new FirefoxDriver();
+		driver = new EdgeDriver();
 		driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 		 //driver.manage().window().setSize(new Dimension(375, 667);//
