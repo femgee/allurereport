7f6bcf27b0b6cbd0eabc18938b260656fe27a30e
femiBalogun <femgee@gmail.com>
2020-03-31 19:36:27 +0100

    added operating system version in the environment.xml

--- src/test/java/allureReports/BaseClass.java
+++ src/test/java/allureReports/BaseClass.java
@@ -3,9 +3,8 @@ package allureReports;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 //import org.openqa.selenium.edge.EdgeDriver;//
-//import org.openqa.selenium.firefox.FirefoxDriver;//
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -16,8 +15,8 @@ public class BaseClass {
 
 	public WebDriver initialize_driver() {
 
-		WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver();
+		WebDriverManager.firefoxdriver().setup();
+		driver = new FirefoxDriver();
 		driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 		 //driver.manage().window().setSize(new Dimension(375, 667);//
